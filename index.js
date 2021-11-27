4c346b46acc9a790c9b59d365e966a0ceb131bcf
Kanchan Mangrule <92220105+KanchanMangrule@users.noreply.github.com>
2021-11-27 15:08:53 +0530

    CoronaVirus Visualisation on World Map
    Plotting CoronaVirus Cases On World Map - Interactive Dashboard Using JavaScript & MapBox .
    I have added 2 json files to show data named as data.json and latestdata.json and commented it's function in index.js , choose wisely .

---
+++ index.js
@@ -0,0 +1,35 @@
+function updateMap()
+{
+    var i = 0; //Comment it for data.json
+    fetch("/latestdata.json")    
+    .then(response => response.json())
+    .then(rsp => {
+    //  console.log(rsp.data)
+        rsp.forEach(element => {  //Will have to change rsp.forEach to rsp.data.forEach for data.json
+            // latitude = element.coordinates;
+            // longitude = element.longitude;
+            // cases = element.infected;
+            latitude = element.coordinates[1];  
+            longitude = element.coordinates[0];
+            cases = element.cases;
+            if(cases>255){
+                color = "rgb(255, 0, 0)" 
+            }
+            else{
+                color = `rgb(${cases}, 0, 0)`
+            }
+            
+            //Mark on the map
+            if(i<1000){  //Use this condition to limit the no. of data otherwise remove the condition for data.json
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                .setLngLat([longitude, latitude])
+                .addTo(map);
+            }
+            i = i+1; //Comment it for data.json
+        });
+    })
+}
+updateMap();
